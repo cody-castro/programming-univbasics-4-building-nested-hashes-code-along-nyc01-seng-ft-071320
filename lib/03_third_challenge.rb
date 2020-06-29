a55534953a056f8015d37f0e5901a737e5142603
Cody C <codyicastro@gmail.com>
2020-06-29 20:06:40 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,4 +1,4 @@
-epic_tragedy = {
+def epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -12,4 +12,4 @@ epic_tragedy = {
       :heroine_friends => []
    }
 }
-  
+end
